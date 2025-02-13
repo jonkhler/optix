e52a6cc6259761706863feaaea5215b714920794
Jonas Köhler <jonas@cusp.ai>
2025-02-13 13:38:15 +0100

    array lenses

--- src/optix/__init__.py
+++ src/optix/__init__.py
@@ -1,11 +1,33 @@
 from __future__ import annotations
 
 from collections.abc import Callable
+from typing import Protocol
 
 import equinox as eqx
+import jax
 
 
-__all__ = ["Lens", "FreeLens", "Lens", "Focused", "focus"]
+__all__ = ["Lens", "BoundLens", "FreeLens", "BoundLens", "Focused", "focus"]
+
+
+
+
+class Lens[T, S](Protocol):
+    """ A protocol for lenses. """
+
+    def get(self) -> S:
+        """ Get the value of the focus in the object. """
+        ...
+
+    def set(self, val: S) -> T:
+        """ Set the value of the focus in the object. """
+        ...
+
+    def apply(self, update: Callable[[S], S]) -> T:
+        """ Apply a function to the focused value in the object. """
+        ...
+
+
 
 class FreeLens[T, S](eqx.Module):
     """ A lens that focuses on a value in an object.
@@ -15,44 +37,8 @@ class FreeLens[T, S](eqx.Module):
     """
 
     where: Callable[[T], S]
-
-    def get(self, obj: T) -> S:
-        """ Get the value of the focus in the object.
-
-        Args:
-            obj: The object to query.
-        
-        Returns:
-            The focused value.
-        """
-            
-        return self.where(obj)
     
-    def set(self, obj: T, val: S) -> T:
-        """ Set the value of the focus in the object.
-
-        Args:
-            obj: The object to modify.
-            val: The new value to set.
-
-        Returns:
-            A modified copy of the object.
-        """
-        return eqx.tree_at(self.where, obj, val)
-    
-    def apply(self, obj: T, update: Callable[[S], S]) -> T:
-        """ Apply a function to the focused value in the object.
-
-        Args:
-            update: The object to modify.
-            new: The function to apply to the focused value.
-            
-        Returns:
-            A modified copy of the object.
-        """
-        return eqx.tree_at(self.where, obj, replace=update(self.get(obj)))
-    
-    def bind(self, obj: T) -> Lens[T, S]:
+    def bind(self, obj: T) -> BoundLens[T, S]:
         """ Bind the lens to an object.
 
         Args:
@@ -61,10 +47,10 @@ class FreeLens[T, S](eqx.Module):
         Returns:
             A bound lens.
         """
-        return Lens(obj, self.where)
+        return BoundLens(obj, self.where)
 
 
-class Lens[T, S](eqx.Module):
+class BoundLens[T, S](eqx.Module):
     """ A lens that focuses on a value in a bound object.
 
     Args:
@@ -106,6 +92,24 @@ class Lens[T, S](eqx.Module):
         return eqx.tree_at(self.where, self.obj, replace=update(self.get()))
     
 
+
+class ArrayLens[T, I](eqx.Module):
+
+    lens: Lens[T, jax.Array]
+    index: I
+
+    def get(self, **kwargs) -> jax.Array:
+        arr = self.lens.get()
+        return arr.at[self.index].get(**kwargs)
+    
+    def set(self, val: jax.Array, **kwargs) -> T:
+        return self.lens.apply(lambda arr: arr.at[self.index].set(val, **kwargs))
+    
+    def apply(self, update: Callable[[jax.Array], jax.Array], **kwargs) -> T:
+        return self.lens.apply(lambda arr: arr.at[self.index].apply(update, **kwargs))
+
+
+
 class Focused[T](eqx.Module):
     """ An object that can be focused on.
 
@@ -123,7 +127,19 @@ class Focused[T](eqx.Module):
         Returns:
             A bound lens.
         """
-        return Lens(self.obj, where)
+        return BoundLens(self.obj, where)
+
+    def at_index[I](self, where: Callable[[T], jax.Array], index: I) -> ArrayLens[T, jax.Array]:
+        """ Focus on an index in an array in the object.
+
+        Args:
+            where: A function that retrieves the focused value from the object.
+            index: The index to focus on.
+
+        Returns:
+            A bound lens.
+        """
+        return ArrayLens(self.at(where), index)
 
 
 def focus[T](obj: T) -> Focused[T]:
